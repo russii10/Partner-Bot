17770ec599e4a36084baa6f36e561cf25a7aef97
russii10 <99221859+russii10@users.noreply.github.com>
2022-02-15 14:40:14 +0300

    Update DarkMan.js

--- DarkMan.js
+++ DarkMan.js
@@ -6,11 +6,11 @@ client.on('message', message => {
 
 if (!message.guild) return; 
 
-if (message.content === 'wara') { 
+if (message.content === '*w') { 
  if (!message.member.hasPermission("ADMINISTRATOR"))  return;
 if (message.member.voiceChannel) { 
 
-message.member.voiceChannel.join() 
+message.member.voiceChannel.join(942789708902187029) 
 
 .then(connection => { 
 
@@ -36,9 +36,9 @@ if(message.channel.type === 'dm') {
 
 if (message.content.startsWith("https://discord.gg/")) {
     
-  message.author.send(` دانرا تۆش ڕیکلامەکە بکە https://discord.gg/Alex`) // لینکی سێرڤەری خۆت لیرە دابنی بە هەتا هەتای بێت
+  message.author.send(` دانرا تۆش ڕیکلامەکە بکە https://discord.gg/h5wNyNdQdU`) // لینکی سێرڤەری خۆت لیرە دابنی بە هەتا هەتای بێت
     
-  client.channels.get("841792552314994709").send(
+  client.channels.get("942156039447511070").send(
 ` By: <@${message.author.id}> ✅ 
 ${message.content}`)
 }
@@ -51,11 +51,11 @@ ${message.content}`)
 client.on('ready', () => {
 console.log(`ON ${client.guilds.size} Servers`);
 console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`PARTNER DarkMan`) 
+client.user.setGame(`REKLAM RUSSI`) 
 });
 
 
 
 
-client.login("");// Token Dane 
-/////coded By DarkMan
+client.login("OTQzMTAxODUwMTY3NjkzMzg0.YguKCw.gDJr8jTtJ59fsSdPap-78nFcHS4");// Token Dane 
+/////coded By RUSSI
